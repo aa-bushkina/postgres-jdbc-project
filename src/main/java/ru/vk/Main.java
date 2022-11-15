6eb1c37d6dd25ae9113f3370bed1c52afdf126c4
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-16 01:49:10 +0300

    reformat code

--- src/main/java/ru/vk/Main.java
+++ src/main/java/ru/vk/Main.java
@@ -12,22 +12,22 @@ public class Main
   {
     final Injector injector = Guice.createInjector(new ApplicationModule(args));
     injector.getInstance(Application.class).makeDB();
-    System.out.println("Выбрать первые 10 поставщиков по количеству поставленного товара\n");
 
+    System.out.println("Выбрать первые 10 поставщиков по количеству поставленного товара\n");
     System.out.println(injector.getInstance(Application.class).getTop10OrganizationByQuantity());
+
     System.out.println("\nВыбрать поставщиков с количеством поставленного товара " +
       "выше указанного значения: 9000\n");
-
     System.out.println(injector.getInstance(Application.class).getOrganizationsWithDefiniteQuantity());
+
     System.out.println("\nЗа каждый день для каждого товара рассчитать количество и " +
       "сумму полученного товара в указанном периоде\n");
-
     System.out.println(injector.getInstance(Application.class).getEverydayProductCharacteristics());
+
     System.out.println("\nРассчитать среднюю цену полученного товара за период\n");
-
     System.out.println(injector.getInstance(Application.class).getAverageOfProductPrice());
-    System.out.println("\nВывести список товаров, поставленных организациями за период\n");
 
+    System.out.println("\nВывести список товаров, поставленных организациями за период\n");
     System.out.println(injector.getInstance(Application.class).getProductsListByOrganizations());
   }
 }
