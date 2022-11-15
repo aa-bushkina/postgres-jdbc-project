8a7316e1575f800abf1d654746e52bdaea90564d
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-16 02:57:19 +0300

    add hamcrest dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     implementation("org.postgresql:postgresql:42.5.0")
     implementation("org.jetbrains:annotations:23.0.0")
     implementation("com.google.inject:guice:5.1.0")
-    testImplementation("org.mockito:mockito-core:4.8.1")
+    testImplementation("org.hamcrest:hamcrest-all:1.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
