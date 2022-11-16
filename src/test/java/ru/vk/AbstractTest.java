1e21a033a862b9dfce6ddbd6dcc7152d00fdefdd
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-16 03:12:57 +0300

    made abstract test clacc

---
+++ src/test/java/ru/vk/AbstractTest.java
@@ -0,0 +1,25 @@
+package ru.vk;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import ru.vk.application.Application;
+import ru.vk.application.ApplicationModule;
+
+public abstract class AbstractTest
+{
+  @NotNull
+  final String[] args = {"jdbc:postgresql://localhost/", "testTask", "postgres", "postgres"};
+
+  @NotNull
+  final String path = "db.queriesTest";
+
+  @BeforeEach
+  public void beforeEach()
+  {
+    Injector injector = Guice.createInjector(new ApplicationModule(args));
+    injector.injectMembers(this);
+    injector.getInstance(Application.class).makeDB(path);
+  }
+}
